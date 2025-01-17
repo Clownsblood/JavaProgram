98091c232c0788be2531eb1d13b905222cb2a75f
phantom <phantom@qq.com>
2025-01-17 15:37:41 +0800

    笔记本端测试

--- src/com/phantom/Exercise/ExerciseSystem.java
+++ src/com/phantom/Exercise/ExerciseSystem.java
@@ -3,5 +3,6 @@ package com.phantom.Exercise;
 public class ExerciseSystem {
     public static void main(String[] args) {
         System.out.println("测试程序");
+        System.out.println("ceshi");
     }
 }
